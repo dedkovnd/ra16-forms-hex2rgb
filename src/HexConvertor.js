4d3821e0ea8df7a25ec9457b982de67ab271a399
Nikita Dedkov <dedkovnd@gmail.com>
2020-09-23 18:59:00 +0300

    fixed HexConvertor

--- src/HexConvertor.js
+++ src/HexConvertor.js
@@ -6,19 +6,27 @@ export function Convertor(props) {
         name: "#22aa11"
     });
 
+    const [formValue, setFormValue] = useState({
+        name: "#22aa11"
+    });
+
     const handleSubmit = evt => {
         evt.preventDefault()
         console.log(form.name)
     }
     const handleChangeSubmit = evt => {
         evt.persist()
-        setForm(prevForm=>({...prevForm, name: evt.target.value}))
+        if(evt.target.value.length === 7) {
+            setForm(prevForm=>({...prevForm, name: evt.target.value}))
+        }
+        setFormValue(prevForm=>({...prevForm, name: evt.target.value}))
+
     }
     return (
         <div className="back" style={{backgroundColor: form.name}}>
             <div className="main">
                 <form className="hex-form" onSubmit={handleSubmit}>
-                    <input id="hex" name="hex" value={form.name} onChange={handleChangeSubmit}/>
+                    <input id="hex" name="hex" value = {formValue.name} onChange={handleChangeSubmit}/>
                 </form>
                 <ScreenColor rgbstring={form.name}/>
             </div>
